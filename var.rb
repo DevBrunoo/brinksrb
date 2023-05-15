cbaebdb70bc49c4c99431ebd48005760f8997ef5
Bruno Oliveira <brunocosta1699320@gmail.com>
2023-05-15 10:04:57 -0300

    add

---
+++ var.rb
@@ -0,0 +1,13 @@
+# Declaração e atribuição de variáveis
+nome = "João"
+idade = 25
+saldo_bancario = 1000.50
+
+# Exibindo o valor das variáveis na tela
+puts "Nome: #{nome}"
+puts "Idade: #{idade}"
+puts "Saldo Bancário: R$ #{saldo_bancario}"
+
+# Alterando o valor de uma variável
+saldo_bancario -= 500.75
+puts "Novo Saldo Bancário: R$ #{saldo_bancario}"
